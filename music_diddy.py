f0bbb37f13105c7e2537bb5b7417f9cb48cf3db5
jedwilshire <jedwilshire@gmail.com>
2021-06-08 14:24:54 -0500

    added notes to music_diddy.py

--- music_diddy.py
+++ music_diddy.py
@@ -4,10 +4,13 @@ from time import sleep
 A = Tone('A4')
 B = Tone('B4')
 C = Tone('C5')
+D = Tone('D5')
 E = Tone('E5')
+F = Tone('F5')
 
-notes = [A, B, C, B, A, A, B, C, E, B, C, A]
-lengths = [.25, .25, 0.5, 0.5, 0.5, .25, .25, .25, .25, .25, .25, .5]
+
+notes = [A, B, C, B, A, A, B, C, E, B, C, A, D]
+lengths = [.25, .25, 0.5, 0.5, 0.5, .25, .25, .25, .25, .25, .25, .5, 1.0]
 tb = TonalBuzzer('BOARD12')
 for i in range(len(notes)):
     tb.play(notes[i])
